9c5a312852f7977c36716bf6fc789a6e11e55be4
Ruben Giuriato <37040971+Rubbonn@users.noreply.github.com>
2022-10-22 16:31:34 +0200

    Potenziamento ThreadManager
    - Aggiunto controllo per non far aggiungere due volte lo stesso thread
    - Restituito l'indice del thread appena aggiunto
    - Aggiunte funzioni per avviare e stoppare un solo thread
    - Sistemata ricreazione del thread

--- classes/threads/ThreadManager.py
+++ classes/threads/ThreadManager.py
@@ -1,31 +1,51 @@
 from threading import Thread, Event
 class ThreadManager:
-	__shutdownEvent: Event = Event()
 	__threadList: list[dict] = []
 	
-	def addThread(self, f: callable, start: bool = False, *args) -> None:
-		args = (self.__shutdownEvent,) + args
-		self.__threadList.append({'target': f, 'args': args, 'thread': Thread(target=f, args=args, daemon=True)})
+	def addThread(self, f: callable, start: bool = False, *args) -> int:
+		for i, t in enumerate(self.__threadList):
+			if f is t['target'] and args is t['args']:
+				if start:
+					self.startThread(i)
+				return i
+		shutdownEvent: Event = Event()
+		self.__threadList.append({'target': f, 'shutdownEvent': shutdownEvent, 'args': args, 'thread': Thread(target=f, args=(shutdownEvent,) + args, daemon=True)})
+		indice: int = len(self.__threadList) - 1
 		if start:
-			self.__shutdownEvent.clear()
-			self.__threadList[:-1]['thread'].start()
+			self.startThread(indice)
+		return indice
+	
+	def startThread(self, indice: int) -> None:
+		if indice < 0 or indice >= len(self.__threadList):
+			return
+		if self.__threadList[indice]['thread'].is_alive():
+			return
+		try:
+			self.__threadList[indice]['shutdownEvent'].clear()
+			self.__threadList[indice]['thread'].start()
+		except RuntimeError:
+			self.__threadList[indice]['thread'] = Thread(target=self.__threadList[indice]['target'], args=(self.__threadList[indice]['shutdownEvent'],) + self.__threadList[indice]['args'], daemon=True)
+			self.__threadList[indice]['shutdownEvent'].clear()
+			self.__threadList[indice]['thread'].start()
 	
 	def startThreads(self) -> None:
-		self.__shutdownEvent.clear()
-		for t in self.__threadList:
-			if not t['thread'].is_alive():
-				t['thread'].start()
+		for i, t in enumerate(self.__threadList):
+			self.startThread(i)
+	
+	def stopThread(self, indice: int):
+		if indice < 0 or indice >= len(self.__threadList):
+			return
+		if not self.__threadList[indice]['thread'].is_alive():
+			return
+		self.__threadList[indice]['shutdownEvent'].set()
+		self.__threadList[indice]['thread'].join()
 	
 	def stopThreads(self) -> None:
-		self.__shutdownEvent.set()
-		for t in self.__threadList:
-			if t['thread'].is_alive():
-				t['thread'].join()
+		for i, t in enumerate(self.__threadList):
+			self.stopThread(i)
 	
 	def restartThreads(self) -> None:
 		self.stopThreads()
-		newList: list[dict] = []
 		for t in self.__threadList:
-			newList.append({'target': t['target'], 'args': t['args'], 'thread': Thread(target=t['target'], args=t['args'], daemon=True)})
-		self.__threadList = newList
+			t['thread'] = Thread(target=t['target'], args=(t['shutdownEvent'],) + t['args'], daemon=True)
 		self.startThreads()
